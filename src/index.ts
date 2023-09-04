0cfb5caa9bb6b3014896ab16043ba6f5799496a9
Ace Paulo Cotejo <acecutejo@gmail.com>
2023-09-04 13:09:25 +0800

    vercil config 2

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ app.listen(PORT, () => {
   console.log(`now listening on port ${PORT}`)
 })
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+
 process.once('SIGUSR2', () => {
   process.kill(process.pid, 'SIGUSR2')
 })
@@ -17,3 +26,5 @@ process.once('SIGUSR2', () => {
 process.on('SIGINT', () => {
   process.kill(0)
 })
+
+export default app;

3696ba54c2eac1f39b73d7789de5e0776cfaf2df
Ace Paulo Cotejo <acecutejo@gmail.com>
2023-09-04 18:26:23 +0800

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-
+import cors from 'cors'
 import { app } from './config/server.js'
 
 dotenv.config()
@@ -10,14 +10,7 @@ app.listen(PORT, () => {
   console.log(`now listening on port ${PORT}`)
 })
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-  // Pass to next layer of middleware
-  if (req.method === 'OPTIONS') res.sendStatus(200);
-  else next();
-});
+app.use(cors())
 
 process.once('SIGUSR2', () => {
   process.kill(process.pid, 'SIGUSR2')
